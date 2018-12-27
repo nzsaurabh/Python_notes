cc15fb21329b8005b19809a716506859bf669a3b
Saurabh Gupta <nzsaurabh@users.noreply.github.com>
2018-12-27 21:10:37 +1300

    Create global_var.py

---
+++ global_var.py
@@ -0,0 +1,53 @@
+#! python3
+
+#quiz:
+
+x = 1
+
+def test():
+    x = 2
+
+test()
+print(x)
+#1 correct
+
+
+x = 1
+
+def test():
+    global x
+    x = 2
+
+test()
+print(x)
+#2 correct
+
+
+x = [1]
+
+def test():
+    x = [2]
+
+test()
+print(x)
+# [1] correct
+
+x = [1]
+
+def test():
+    global x
+    x = [2]
+
+test()
+print(x)
+# [2] correct
+
+
+x = [1]
+
+def test():
+    x[0] = 2
+
+test()
+print(x)
+# [2] correct
